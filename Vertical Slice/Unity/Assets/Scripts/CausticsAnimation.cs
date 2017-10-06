3148f1de7d112262d6f090b9bfc6b0a370fa5b16
Unknown <nderey6@gmail.com>
2017-10-06 01:32:40 -0600

    Realized that I may have been overthinking caustics. Watched some videos about how to animate UVs. Tried a new script and got the UV animation to work, but the caustic graphics are still not flowing through the animation properly. It's very choppy and jump around too much. Will try and smooth it out.

---
+++ Vertical Slice/Unity/Assets/Scripts/CausticsAnimation.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CausticsAnimation : MonoBehaviour 
+{
+	public int tileColumn = 4;
+	public int tileRow = 2;
+	public int frames = 30;
+	private int index;
+	private Vector2 scale;
+
+	// Update is called once per frame
+	void Update () 
+	{
+		// Calculates how the animation will run
+		index = (int)(Time.time * frames);
+
+		// Repeat the calculation once all frames have been exhausted
+		index = index % (tileColumn * tileRow);
+
+		// Calculate how big each tile is
+		scale = new Vector2(1.0f / tileColumn, 1.0f / tileRow);
+
+		// Divide the columns and rows into seperate sections
+		var uRow = index % tileRow;
+		var uColumn = index / tileColumn;
+
+		Vector2 offset = new Vector2 (uRow * scale.x, 1.0f - scale.y - uColumn * scale.y);
+
+		GetComponent<Renderer>().material.SetTextureOffset ("_MainTex", offset);
+		GetComponent<Renderer>().material.SetTextureScale ("_MainTex", scale);
+	}
+}
