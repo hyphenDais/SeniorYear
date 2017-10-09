b793860e2e4f8ee3b2cb0fe712d1a1334db73647
Unknown <nderey6@gmail.com>
2017-10-09 15:03:59 -0600

    Added new scenes to Unity

---
+++ Vertical Slice/Unity/Assets/Scripts/BatteryLife.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BatteryLife : MonoBehaviour 
+{
+	public float maxBattery = 100.0f;
+	public float halfBattery = 50.0f;
+	public float lowBattery = 25.0f;
+	public Text batteryText; 
+
+	// Use this for initialization
+	void Start () 
+	{
+		SetBatteryText();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKey (KeyCode.X)) 
+		{
+			BatteryDrain ();
+		}
+
+		maxBattery -= Time.deltaTime * 2.0f;
+		SetBatteryText();
+	}
+
+	void BatteryDrain()
+	{
+		if (maxBattery > halfBattery) 
+		{
+			print ("Battery Power is optimal.");
+		} 
+		else if (maxBattery > lowBattery) 
+		{
+			print ("Battery Power is sufficient.");
+		}
+		else
+		{
+			print ("Battery Power is low.");
+		}
+	}
+
+	void SetBatteryText()
+	{
+		batteryText.text = "Battery Power: " + maxBattery.ToString ();
+	}
+}
